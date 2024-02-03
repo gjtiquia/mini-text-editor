91c07f900c00b0ed845c325f4a82274da89ad253
GJ Tiquia <gershom.tiquia@gmail.com>
2024-02-03 15:44:38 +0800

    WIP: express returns file contents

--- apps/file-server/src/index.ts
+++ apps/file-server/src/index.ts
@@ -12,18 +12,20 @@ app.use(cors()); // Enable all origins
 
 // Set Routes
 // app.use("/app", appRouter)
-app.get("/", (req, res) => res.send("Hello World!"))
 
-app.listen(PORT, () => {
-    console.log(`Server is running at: http://localhost:${PORT}`);
-    console.log('Press Ctrl+C to quit.');
-});
+app.get("/", async (req, res) => {
 
-async function main() {
     const directoryPath = process.cwd();
     const { directoryContents, fileContents } = await getDirectoriesAndFilesAsync(directoryPath);
 
     console.log("path", directoryPath);
     console.log("directories", directoryContents.map(x => x.name))
     console.log("files", fileContents.map(x => x.name))
-}
+
+    res.send(JSON.stringify({ directoryContents, fileContents }))
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running at: http://localhost:${PORT}`);
+    console.log('Press Ctrl+C to quit.');
+});
