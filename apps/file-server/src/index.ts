1c6fc6ec06476f99213d6f8e77518cd43dd225e0
GJ Tiquia <gershom.tiquia@gmail.com>
2024-02-03 15:42:05 +0800

    feat: basic express server setup

--- apps/file-server/src/index.ts
+++ apps/file-server/src/index.ts
@@ -1,12 +1,26 @@
+import express from "express"
+import cors from "cors";
 import { getDirectoriesAndFilesAsync } from "./getDirectoriesAndFiles";
 
-main();
+console.log("Running @mini-text-editor/file-server...")
+
+const app = express();
+const PORT = 3000;
+
+// Global Middleware
+app.use(cors()); // Enable all origins
+
+// Set Routes
+// app.use("/app", appRouter)
+app.get("/", (req, res) => res.send("Hello World!"))
+
+app.listen(PORT, () => {
+    console.log(`Server is running at: http://localhost:${PORT}`);
+    console.log('Press Ctrl+C to quit.');
+});
 
 async function main() {
-    console.log("Running @mini-text-editor/file-server...")
-
     const directoryPath = process.cwd();
-
     const { directoryContents, fileContents } = await getDirectoriesAndFilesAsync(directoryPath);
 
     console.log("path", directoryPath);
